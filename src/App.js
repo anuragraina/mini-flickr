493e4e5ca87d2220e2597d1f6ed8c63d49d58f21
Anurag Raina <rainakalhan90@gmail.com>
2020-12-05 00:19:24 +0530

    Connect Flickr API with app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import Flickr from 'flickr-sdk';
+
+const flickr = new Flickr(process.env.REACT_APP_FLICKR_API_KEY);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		flickr.photos
+			.getInfo({
+				photo_id: 25825763, // sorry, @dokas
+			})
+			.then(function (res) {
+				console.log('yay!', res.body);
+			})
+			.catch(function (err) {
+				console.error('bonk', err);
+			});
+	}, []);
+
+	return <div>dsvfsbf</div>;
 }
 
 export default App;
