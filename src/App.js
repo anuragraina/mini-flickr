00d6e430737ba7848e4ac83f32b92843392afe4d
Anurag Raina <rainakalhan90@gmail.com>
2020-12-05 14:46:43 +0530

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import { useEffect } from 'react';
-import Flickr from 'flickr-sdk';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+//import Flickr from 'flickr-sdk';
 
-const flickr = new Flickr(process.env.REACT_APP_FLICKR_API_KEY);
+import Groups from './views/groups';
+import Gallery from './views/gallery';
+
+//const flickr = new Flickr(process.env.REACT_APP_FLICKR_API_KEY);
 
 function App() {
-	useEffect(() => {
-		flickr.photos
-			.getInfo({
-				photo_id: 25825763, // sorry, @dokas
-			})
-			.then(function (res) {
-				console.log('yay!', res.body);
-			})
-			.catch(function (err) {
-				console.error('bonk', err);
-			});
-	}, []);
-
-	return <div>dsvfsbf</div>;
+	return (
+		<Router>
+			<Switch>
+				<Route path='/groups' component={Groups} />
+				<Route path='/gallery' component={Gallery} />
+				<Route>
+					<Redirect to='/groups' />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
